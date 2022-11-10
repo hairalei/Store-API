b962d3100ccb265b306ccb64eae215fdc22db6ec
Haira Lei <darhaii13@gmail.com>
2022-11-10 20:00:05 +0800

    add numeric filters

--- controllers/products.js
+++ controllers/products.js
@@ -6,11 +6,37 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { name, sort, fields } = req.query;
+  const { name, sort, fields, numericFilters } = req.query;
   const queryObject = req.query;
 
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' };
+  }
+
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    };
+
+    const regEx = /\b(<|>|<=|>=|=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ['price', 'rating'];
+
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-');
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+
+    console.log(queryObject);
   }
 
   let result = Product.find(queryObject);
