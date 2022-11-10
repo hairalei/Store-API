cbcf86b66bee3da6b44d2b49a52feac9688e153a
Haira Lei <darhaii13@gmail.com>
2022-11-10 13:40:34 +0800

    add sort and fields query params

--- controllers/products.js
+++ controllers/products.js
@@ -1,19 +1,35 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({});
+  const products = await Product.find({}).sort('name price');
   res.status(200).json({ nbHits: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
-  const { name } = req.query;
+  const { name, sort, fields } = req.query;
   const queryObject = req.query;
 
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' };
   }
 
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+
+  // sort
+  if (sort) {
+    const sortLlist = sort.split(',').join(' ');
+    result = result.sort(sortLlist);
+  } else {
+    result = result.sort('createdAt');
+  }
+
+  // fields
+  if (fields) {
+    const fieldsLlist = fields.split(',').join(' ');
+    result = result.select(fieldsLlist);
+  }
+
+  const products = await result;
   res.status(200).json({ nbHits: products.length, products });
 };
 
