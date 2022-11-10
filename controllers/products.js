752b910ab23b1b8f597b8f5ecb6e079a9092e98d
Haira Lei <darhaii13@gmail.com>
2022-11-10 14:34:38 +0800

    add pagination feature

--- controllers/products.js
+++ controllers/products.js
@@ -29,6 +29,12 @@ const getAllProducts = async (req, res) => {
     result = result.select(fieldsLlist);
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
   res.status(200).json({ nbHits: products.length, products });
 };
